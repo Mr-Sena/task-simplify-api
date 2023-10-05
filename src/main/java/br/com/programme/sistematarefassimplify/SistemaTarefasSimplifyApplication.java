8b0c67184e50082587b9fc63cdc39a7952bdca07
Mr-Sena <jsenna985@gmail.com>
2023-10-05 16:31:43 -0300

    Play Sprint Boot

---
+++ src/main/java/br/com/programme/sistematarefassimplify/SistemaTarefasSimplifyApplication.java
@@ -0,0 +1,13 @@
+package br.com.programme.sistematarefassimplify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTarefasSimplifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTarefasSimplifyApplication.class, args);
+	}
+
+}
