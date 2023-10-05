83a4b1dacf83d681005ddf88a16b0f4c413926b6
Mr-Sena <jsenna985@gmail.com>
2023-10-05 16:30:40 -0300

    service endpoint

---
+++ src/main/java/br/com/programme/sistematarefassimplify/controller/TaskController.java
@@ -0,0 +1,51 @@
+package br.com.programme.sistematarefassimplify.controller;
+
+import br.com.programme.sistematarefassimplify.core.model.Tarefa;
+import br.com.programme.sistematarefassimplify.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService service;
+    
+
+    @PostMapping
+    ResponseEntity create(@RequestBody Tarefa task) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.newTask(task));
+    }
+
+
+
+    @GetMapping
+    List<Tarefa> list() {
+        return service.listAll();
+    }
+
+
+    @PutMapping("{id}")
+    ResponseEntity<Tarefa> update (@PathVariable Long id, @RequestBody Tarefa task) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(service.atualizar(id, task));
+
+    }
+
+
+    @DeleteMapping("/{id}")
+    ResponseEntity delete (@PathVariable Long id) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(service.remove(id));
+
+    }
+
+}
---
+++ src/main/java/br/com/programme/sistematarefassimplify/service/TaskService.java
@@ -0,0 +1,59 @@
+package br.com.programme.sistematarefassimplify.service;
+
+import br.com.programme.sistematarefassimplify.core.model.Tarefa;
+import br.com.programme.sistematarefassimplify.infra.repo.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private TaskRepository repository;
+
+    public TaskService(TaskRepository repository) {
+        this.repository = repository;
+    }
+
+
+    public Tarefa newTask(Tarefa task) {
+        return repository.save(task);
+    }
+
+    public List<Tarefa> listAll() {
+
+        Sort sort = Sort.by("prioridade").descending()
+                .and(Sort.by("nome").ascending());
+
+        return repository.findAll(sort);
+    }
+
+    public Tarefa atualizar(Long id, Tarefa task) {
+
+        repository.findById(id).ifPresentOrElse((existingTask) -> {
+            task.setIdTable(id);
+            repository.save(task);
+        }, () -> { // Todo: Create Badrequest exception.
+            throw new RuntimeException("Badquest -> Task %d não existe! ".formatted(id));
+        });
+
+        return task;
+    }
+
+    public Object remove(Long id) {
+
+        var task = repository.findById(id);
+
+        if (task.isPresent()) {
+            repository.deleteById(id);
+            return task.get();
+
+        }
+
+        return "Task %d não existe!".formatted(id);
+
+
+    }
+}
